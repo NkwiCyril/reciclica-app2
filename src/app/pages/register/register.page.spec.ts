d35d83375e059d1b865c5642145f38e1f4343bcd
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-07-11 14:34:57 +0100

    all pages set for testing

--- src/app/pages/register/register.page.spec.ts
+++ src/app/pages/register/register.page.spec.ts
@@ -1,21 +1,3 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed} from '@angular/core/testing';
 import { RegisterPage } from './register.page';
 
-describe('RegisterPage', () => {
-  let component: RegisterPage;
-  let fixture: ComponentFixture<RegisterPage>;
-
-  beforeEach(async(() => {
-    fixture = TestBed.createComponent(RegisterPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
-
-function async(arg0: () => void): jasmine.ImplementationCallback {
-  throw new Error('Function not implemented.');
-}
